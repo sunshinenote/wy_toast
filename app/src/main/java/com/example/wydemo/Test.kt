d5fc15bd302d0c3bc747007c7e1959bb673ebad2
wenyang <wenyang@cloudwalk.com>
2022-08-09 16:24:27 +0800

    test

---
+++ app/src/main/java/com/example/wydemo/Test.kt
@@ -0,0 +1,10 @@
+package com.example.wydemo
+
+import android.content.Context
+import com.example.wytoast.WyToast
+
+object Test {
+    fun test(context: Context) {
+        WyToast.showToast(context)
+    }
+}
